c277dd576c8b1ececeeabe9f3a99feed200754c0
ar-hosseinkhani <alireza.8096@chmail.ir>
2023-06-28 23:34:54 +0330

    feat(server): start server apis

---
+++ sever/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`one-piece-card-game server is running on port: ${port}`)
+})
